b77f946ba16343a41180e83b68a9a15dd71ca32f
veyis <vakgul@gmail.com>
2024-04-17 12:33:29 -0700

    v3

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -20,9 +20,9 @@ const AboutUs = () => {
 					</div>
 					<div className="md:col-span-7 text-lg font-light text-gray-700 space-y-4">
 						<div class="container">
-							<h1>Hello there!</h1>
+							<h1>Hello there! We are Aleksandra and Randy. </h1>
 							<br />
-							<p>We are Aleksandra and Randy.</p>
+						
 							<p>
 								On our first trip to San Antonio, we fell in love with this
 								city's charm. We were captivated by the enchanting beauty of the
